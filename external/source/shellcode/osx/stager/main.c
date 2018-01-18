24cc3c38453ac45c652480cc418b2b7f2ef0f333
Tim W <timrlw@gmail.com>
2018-01-18 22:11:39 +0800

    cruft

--- external/source/shellcode/osx/stager/main.c
+++ external/source/shellcode/osx/stager/main.c
@@ -9,6 +9,26 @@
 
 #include <sys/types.h>
 
+
+#if __LP64__
+	#define LC_SEGMENT_COMMAND			LC_SEGMENT_64
+	#define LC_ROUTINES_COMMAND			LC_ROUTINES_64
+	typedef struct mach_header_64		macho_header;
+	typedef struct section_64			macho_section;
+	typedef struct nlist_64				macho_nlist;
+	typedef struct segment_command_64	macho_segment_command;
+#else
+	#define LC_SEGMENT_COMMAND		LC_SEGMENT
+	#define LC_ROUTINES_COMMAND		LC_ROUTINES
+	typedef struct mach_header		macho_header;
+	typedef struct section			macho_section;
+	typedef struct nlist			macho_nlist;
+	typedef struct segment_command	macho_segment_command;
+#endif
+
+extern const macho_header __dso_handle;
+
+
 #ifdef __LP64__
 typedef struct mach_header_64 mach_header_t;
 typedef struct segment_command_64 segment_command_t;
@@ -36,6 +56,176 @@ void* stderr_ptr = 0;
 fprintf_ptr fprintf_func = 0;
 dlsym_ptr dlsym_func = 0;
 
+// This function may be overriden by application code
+// to do custom error handling when a lazy symbol cannot be  
+// resolved.
+int dyld_lazy_dylib_proxy() __attribute__((weak,visibility("hidden")));
+int dyld_lazy_dylib_proxy()
+{
+	return 0;
+}
+
+
+// This function may be overriden by application code
+// to dynamically change the path to a loaded lazy dylib. 
+const char* dyld_lazy_dylib_path_fix(const char*) __attribute__((weak,visibility("hidden")));
+const char* dyld_lazy_dylib_path_fix(const char* path)
+{
+	return path;
+}
+
+
+static void* getHandleForLazyOrdinal(const macho_header* mh, void* handles[], uint8_t ordinal)
+{
+	const uint32_t cmd_count = mh->ncmds;
+	const struct load_command* const cmds = (struct load_command*)((char*)mh + sizeof(macho_header));
+	const struct load_command* cmd = cmds;
+	uint8_t loadDylibCount = 0;
+	uint8_t loadLazyDylibCount = 0;
+	uint32_t i;
+	// walk load commands to find LC_LAZY_LOAD_DYLIB that matches ordinal
+	for (i = 0; i < cmd_count; ++i) {
+		switch ( cmd->cmd ) {
+			case LC_LOAD_DYLIB:
+			case LC_LOAD_WEAK_DYLIB:
+			case LC_LOAD_UPWARD_DYLIB:
+				++loadDylibCount;
+				break;
+			case LC_LAZY_LOAD_DYLIB:
+				++loadDylibCount;
+				if ( loadDylibCount == ordinal ) {
+					if ( handles[loadLazyDylibCount] == NULL ) {
+						const struct dylib_command* dylib = (struct dylib_command*)cmd;
+						const char* path = (char*)cmd + dylib->dylib.name.offset;
+						const char* fixedPath = dyld_lazy_dylib_path_fix(path);
+						handles[loadLazyDylibCount] = dlopen(fixedPath, RTLD_LAZY);
+					}
+					return handles[loadLazyDylibCount];
+				}
+				++loadLazyDylibCount;
+				break;
+		}
+		cmd = (const struct load_command*)(((char*)cmd)+cmd->cmdsize);
+	}
+	return NULL;
+}
+
+
+
+// called by dyld_lazy_dylib_stub_binding_helper
+// this function must figure out which function
+// lazyPointer is supposed to point to 
+// and update it it.
+void* lazy_load_dylib(uintptr_t* lazyPointer)
+{
+	static const macho_header*	mh = NULL;
+	static const macho_nlist*	symbolTable = NULL;
+	static const char*			stringTable = NULL;
+	static const uint8_t*		linkEditBase = NULL;
+	static const uint32_t*		indirectSymbolTable = NULL;
+	static intptr_t				slide = 0;
+	static void*				handles[256];
+	
+	// do this work only on first call
+	uint32_t i;
+	if ( mh == NULL ) {
+		const macho_header* tmh = &__dso_handle;
+		fprintf_func(stderr_ptr, "%s %p\n", "go", tmh);
+		// symbol table, indirect symbol table
+		const uint32_t cmd_count = tmh->ncmds;
+		const struct load_command* const cmds = (struct load_command*)((char*)tmh + sizeof(macho_header));
+		const struct load_command* cmd = cmds;
+		// first pass at load commands gets linkEditBase
+		for (i = 0; i < cmd_count; ++i) {
+			if ( cmd->cmd == LC_SEGMENT_COMMAND ) {
+				const macho_segment_command* seg = (macho_segment_command*)cmd;
+				if ( strcmp(seg->segname,"__TEXT") == 0 ) 
+					slide = (uintptr_t)tmh - seg->vmaddr;
+				else if ( strcmp(seg->segname,"__LINKEDIT") == 0 ) 
+					linkEditBase = (uint8_t*)(seg->vmaddr + slide - seg->fileoff);
+			}
+			cmd = (const struct load_command*)(((char*)cmd)+cmd->cmdsize);
+		}
+		// next pass at load commands gets symbolTable, stringTable
+		uint32_t lazyDylibCount = 0;
+		cmd = cmds;
+		for (i = 0; i < cmd_count; ++i) {
+			switch ( cmd->cmd ) {
+				case LC_SYMTAB:
+					{
+						const struct symtab_command* symtab = (struct symtab_command*)cmd;
+						stringTable = (const char*)&linkEditBase[symtab->stroff];
+						symbolTable = (macho_nlist*)(&linkEditBase[symtab->symoff]);
+					}
+					break;
+				case LC_DYSYMTAB:
+					{
+						const struct dysymtab_command* dsymtab = (struct dysymtab_command*)cmd;
+						indirectSymbolTable = (uint32_t*)(&linkEditBase[dsymtab->indirectsymoff]);
+					}
+					break;
+				case LC_LAZY_LOAD_DYLIB:
+					++lazyDylibCount;
+					break;
+			}
+			cmd = (const struct load_command*)(((char*)cmd)+cmd->cmdsize);
+		}
+		
+		// save to static global to make this thread safe
+		mh = tmh;
+	}
+	
+							/*fprintf_func(stderr_ptr, "lol %p\n", 0);*/
+	return 0;
+	// find lazy dylib pointer section
+	/*void* result = &dyld_lazy_dylib_proxy;*/
+	const uint32_t cmd_count = mh->ncmds;
+	const struct load_command* const cmds = (struct load_command*)((char*)mh + sizeof(macho_header));
+	const struct load_command* cmd = cmds;
+	// walk sections to find one with this lazy pointer
+	for (i = 0; i < cmd_count; ++i) {
+		if ( cmd->cmd == LC_SEGMENT_COMMAND ) {
+			const macho_segment_command* seg = (macho_segment_command*)cmd;
+			const macho_section* const sectionsStart = (macho_section*)((char*)seg + sizeof(macho_segment_command));
+			const macho_section* const sectionsEnd = &sectionsStart[seg->nsects];
+			const macho_section* sect;
+			for (sect=sectionsStart; sect < sectionsEnd; ++sect) {
+				const uint8_t type = sect->flags & SECTION_TYPE;
+							fprintf_func(stderr_ptr, "lol %p %p\n", sect, type);
+				/*if ( type == S_LAZY_DYLIB_SYMBOL_POINTERS ) { // S_LAZY_DYLIB_SYMBOL_POINTERS*/
+        if ((sect->flags & SECTION_TYPE) == S_LAZY_SYMBOL_POINTERS) {
+					const uint32_t pointerCount = sect->size / sizeof(uintptr_t);
+					uintptr_t* const symbolPointers = (uintptr_t*)(sect->addr + slide);
+					if (1 || (lazyPointer >= symbolPointers) && (lazyPointer < &symbolPointers[pointerCount]) ) {
+						const uint32_t indirectTableOffset = sect->reserved1;
+						const uint32_t lazyIndex = lazyPointer - symbolPointers;
+						uint32_t symbolIndex = indirectSymbolTable[indirectTableOffset + lazyIndex];
+							const char* symbolName = &stringTable[symbolTable[symbolIndex].n_un.n_strx];
+							fprintf_func(stderr_ptr, "lol %p %s\n", 1, symbolName);
+						if ( symbolIndex != INDIRECT_SYMBOL_ABS && symbolIndex != INDIRECT_SYMBOL_LOCAL ) {
+							// found symbol for this lazy pointer, now lookup address
+							uint8_t ordinal = GET_LIBRARY_ORDINAL(symbolTable[symbolIndex].n_desc);
+							void* handle = getHandleForLazyOrdinal(mh, handles, ordinal);
+							if ( handle != NULL ) {
+								void* addr = dlsym(handle, &symbolName[1]);
+								/*if ( addr != NULL )*/
+									/*result = addr;*/
+								/**lazyPointer = (uintptr_t)result;*/
+								/*return result;*/
+							}
+						}
+					}
+				}
+			}
+		}
+		cmd = (const struct load_command*)(((char*)cmd)+cmd->cmdsize);
+	}	
+	/**lazyPointer = (uintptr_t)result;*/
+	/*return result;*/
+}
+
+
+void parse_image(const mach_header_t *header, intptr_t slide);
 /*uint64_t find_macho(uint64_t addr, unsigned int increment, unsigned int dereference);*/
 uint64_t find_magic(uint64_t addr, uint32_t magic, unsigned int increment, unsigned int pointer);
 uint64_t find_symbol(uint64_t base, char* symbol);
@@ -103,14 +293,11 @@ int main(int argc, char** argv)
 	fprintf_func(stderr_ptr, "%s %p\n", "dlsym", dlsym_func);
 	fprintf_func(stderr_ptr, "%s %p\n", "dlsym", dlsym_func(RTLD_DEFAULT, "dlsym"));
 
-	uint64_t my_macho = find_magic(0x100000000, 0xfeedfa63, 0x1000, 0);
-	if (!my_macho) {
-		return 1;
-	}
-	fprintf_func(stderr_ptr, "%s %p\n", "my_macho", my_macho);
+	/*set_symbols(my_macho);*/
+	/*set_symbols(my_macho);*/
 
-	/*set_symbols(my_macho);*/
-	/*set_symbols(my_macho);*/
+	lazy_load_dylib(fprintf);
+	print("main!\n");
 
 	fprintf_func(stderr_ptr, "%s %p\n", "fprintf", fprintf);
 	/*rebind_symbols_for_image(my_macho, 0);*/
@@ -120,6 +307,21 @@ int main(int argc, char** argv)
 		_dyld_register_func_for_add_image_func(rebind_symbols_for_image);
 		print("done add image!\n");
 	}
+
+	while (1) {
+		sleep(100);
+	}
+
+	uint64_t my_macho = find_magic(0x100000000, 0xfeedfa63, 0x1000, 0);
+	if (!my_macho) {
+		return 1;
+	}
+	fprintf_func(stderr_ptr, "%s %p\n", "my_macho", my_macho);
+	/*set_symbols(my_macho);*/
+	/*fix_bindings(my_macho);*/
+	fprintf_func(stderr_ptr, "%s %p\n", "done", my_macho);
+	/*set_symbols(buffer);*/
+	fprintf_func(stderr_ptr, "%s %p\n", "buffer", my_macho);
 
 #endif
 #ifdef __x86_64
@@ -199,14 +401,21 @@ static void fix_bindings( section_t *section, intptr_t slide, nlist_t *symtab, c
     }
     uint32_t strtab_offset = symtab[symtab_index].n_un.n_strx;
     char *symbol_name = strtab + strtab_offset;
-			/*print(symbol_name);*/
-			/*print("!!!!\n");*/
-		/*if (indirect_symbol_bindings[i] == fprintf) {*/
+		if (string_compare(symbol_name, "_fprintf") == 0) {
+			fprintf_func(stderr_ptr, "fprintf %p %p %p\n", fprintf, indirect_symbol_bindings[i],  fprintf_func);
+		}
+		fprintf_func(stderr_ptr, "symbol %s %p %p\n", symbol_name, indirect_symbol_bindings[i],  i);
+		if (0 && indirect_symbol_bindings[i] == fprintf) {
+			fprintf(stderr, "testing");
+			fprintf_func(stderr_ptr, "fprintf %p %p %p\n", fprintf, indirect_symbol_bindings[i],  fprintf_func);
+			indirect_symbol_bindings[i] = fprintf_func;
+			fprintf(stderr, "tested");
+			fprintf_func(stderr_ptr, "fprintf %p %p %p\n", fprintf, indirect_symbol_bindings[i],  fprintf_func);
 			/*fprintf(stderr, "%s %p\n", symbol_name, indirect_symbol_bindings[i]);*/
 			/*print("!!!!\n");*/
 			/*print(symbol_name);*/
 			/*print("!!!!\n");*/
-		/*}*/
+		}
 	}
 }
 
@@ -271,8 +480,9 @@ void parse_image(const mach_header_t *header, intptr_t slide)
 			for (uint j = 0; j < cur_seg_cmd->nsects; j++) {
         section_t *sect = (section_t *)(cur + sizeof(segment_command_t)) + j;
         if ((sect->flags & SECTION_TYPE) == S_LAZY_SYMBOL_POINTERS) {
+					fprintf_func(stderr_ptr, "section %p %s %s\n", sect, sect->segname, sect->sectname);
 					/*print("lolol");*/
-					/*fix_bindings(sect, slide, symtab, strtab, indirect_symtab);*/
+					fix_bindings(sect, slide, symtab, strtab, indirect_symtab);
         }
         if ((sect->flags & SECTION_TYPE) == S_NON_LAZY_SYMBOL_POINTERS) {
 					fprintf_func(stderr_ptr, "section %p %s %s\n", sect, sect->segname, sect->sectname);
@@ -285,14 +495,22 @@ void parse_image(const mach_header_t *header, intptr_t slide)
   }
 }
 
+uint64_t set_symbols(uint64_t base);
 void rebind_symbols_for_image(const mach_header_t *header, intptr_t slide)
 {
+		if ((uint64_t)header > 0x7fff00000000) {
+		/*fprintf_func(stderr_ptr, "skip %p %p\n", header, slide);*/
+		/*} else {*/
 		fprintf_func(stderr_ptr, "rebind %p %p\n", header, slide);
+		/*set_symbols(header);*/
 		/*parse_image(header, slide);*/
+		}
 }
 
 uint64_t set_symbols(uint64_t base) 
 {
+	fprintf_func(stderr_ptr, "set_symbols %p %p\n", base, *(uint32_t*)base);
+	/*return 0;*/
 	struct segment_command_64 *sc, *linkedit, *text;
 	struct load_command *lc;
 	struct symtab_command *symtab;
@@ -316,9 +534,9 @@ uint64_t set_symbols(uint64_t base)
 			} else if (string_compare(segname, "__TEXT") == 0) {
 				text = sc;
 			}
-    } else if (lc->cmd == LC_DYSYMTAB) {
-      dysymtab = (struct dysymtab_command*)lc;
-    }
+		} else if (lc->cmd == LC_DYSYMTAB) {
+			dysymtab = (struct dysymtab_command*)lc;
+		}
 		lc = (struct load_command *)((unsigned long)lc + lc->cmdsize);
 	}
 
@@ -330,21 +548,24 @@ uint64_t set_symbols(uint64_t base)
 
 	unsigned long file_slide = linkedit->vmaddr - text->vmaddr - linkedit->fileoff;
 	fprintf_func(stderr_ptr, "file_slide %p %p %p %p\n", file_slide, linkedit->vmaddr, text->vmaddr, linkedit->fileoff);
-	strtab = (char *)(base + file_slide + symtab->stroff);
 	fprintf_func(stderr_ptr, "symtab %p %p %p %p\n", dysymtab, dysymtab->indirectsymoff, dysymtab->nindirectsyms, indirect_symtab);
+	strtab = (char *)(base + file_slide + symtab->stroff);
+	fprintf_func(stderr_ptr, ">>>>>>>>>>>> %p %p %p %p\n", file_slide, strtab, (base + symtab->stroff));
 	/*fprintf_func(stderr_ptr, "symtab %p %p %p %p\n", dysymtab, dysymtab->indirectsymoff, dysymtab->nindirectsyms, *indirect_symtab);*/
+	return 0;
 
 	nl = (struct nlist_64 *)(base + file_slide + symtab->symoff);
 	for (int i=0; i<symtab->nsyms; i++) {
 		char *name = strtab + nl[i].n_un.n_strx;
 		#ifdef DEBUG
-		fprintf_func(stderr_ptr, "symbol %p %p %s %p\n", i, (base + nl[i].n_value), name, nl[i].n_value);
+		fprintf_func(stderr_ptr, "nl symbol %p %s %p %p %p\n", i, name, (base + nl[i].n_value), nl[i].n_sect, nl[i].n_value);
 		#endif
 		/*symbol = base + */
 		if (nl[i].n_value == 0) {
 			void* symbol = dlsym_func(RTLD_DEFAULT, name + 1);
 			if (symbol) {
-				fprintf_func(stderr_ptr, "linking %s %p %p -> %p\n", name, symbol, nl[i].n_value, (symbol - base));
+				/*fprintf_func(stderr_ptr, "linking %s %p %p -> %p\n", name, symbol, nl[i].n_value, (symbol - base));*/
+				/*nl[i].n_sect = 0x1;*/
 				/*nl[i].n_value = symbol - base;*/
 			}
 		}
@@ -355,6 +576,7 @@ uint64_t set_symbols(uint64_t base)
 	}
 
 	fprintf_func(stderr_ptr, "base %p %p %p\n", (void*)base + 0x3000, (base + 0x3000), *(uint64_t*)(base + 0x3000));
+	return 0;
 	/*fprintf(stderr_ptr, "lol\n");*/
 
 	nl = (struct nlist_64 *)(base + file_slide + symtab->symoff);
@@ -366,17 +588,17 @@ uint64_t set_symbols(uint64_t base)
 		/*symbol = base + */
 		if (nl[i].n_value == 0) {
 			void* symbol = dlsym_func(RTLD_DEFAULT, name + 1);
-			if (symbol) {
-				fprintf_func(stderr_ptr, "linking %s %p %p -> %p\n", name, symbol, nl[i].n_value, (symbol - base));
-				nl[i].n_value = symbol - base;
-			}
+			/*if (symbol) {*/
+				/*fprintf_func(stderr_ptr, "linking %s %p %p -> %p\n", name, symbol, nl[i].n_value, (symbol - base));*/
+				/*nl[i].n_value = symbol - base;*/
+			/*}*/
 		}
 
 		/*if (string_compare(name, symbol) == 0) {*/
 			/*return base + nl[i].n_value;*/
 		/*}*/
 	}
-	fprintf(stderr_ptr, "lol\n");
+	/*fprintf(stderr_ptr, "lol\n");*/
 	return 0;
 }
 
